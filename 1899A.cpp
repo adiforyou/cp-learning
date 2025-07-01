a5221af2f5381e564e62367451a520ac169e4268
Aditya Singh <jeevan56k@gmail.com>
2025-07-01 11:26:37 +0530

    1899A.cpp

---
+++ 1899A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using ll =long long;
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if(n%3){
+        cout<<"First"<<endl;
+    }else{
+        cout<<"Second"<<endl;
+    }
+}
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+   int t; cin>>t ;while(t--)
+    solve();
+    
+}
